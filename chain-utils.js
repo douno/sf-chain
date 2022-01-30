b087700683004ddfee12aa342db3d4a3aad0e215
Douno <douno30@gmail.com>
2022-01-30 09:55:15 +0000

    Sign transaction and create input object

--- chain-utils.js
+++ chain-utils.js
@@ -1,4 +1,5 @@
 const EC = require('elliptic').ec;
+const SHA256 = require('crypto-js/sha256');
 const uuid = require('uuid');
 const ec = new EC('secp256k1');
 
@@ -10,6 +11,12 @@ class ChainUtil {
   static id() {
     return uuid.v1();
   }
+
+  static hash(data) {
+    return SHA256(JSON.stringify(data)).toString();
+  }
+
+
 }
 
 module.exports = ChainUtil;
