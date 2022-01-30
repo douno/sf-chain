dd225ffe5a9f69e0965d496bda492ffb2fd6a4a1
Douno <douno30@gmail.com>
2022-01-30 08:45:36 +0000

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const Blockchain = require('../blockchain');
+const P2pServer = require('./p2p-server');
+
+const HTTP_PORT = process.env.HTTP_PORT || 3001;
+
+const app = express();
+const bc = new Blockchain();
+const p2pServer = new P2pServer(bc);
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/blocks', (req, res) => {
+  res.json(bc.chain);
+});
+
+app.post('/mine', (req, res) => {
+  console.log(req.body)
+  const block = bc.addBlock(req.body.data);
+  console.log(`New block added: ${block.toString()}`);
+  p2pServer.syncChains();
+  res.redirect('/blocks');
+});
+
+app.listen(HTTP_PORT, () => console.log(`listening on port ${HTTP_PORT}`));
+p2pServer.listen();
