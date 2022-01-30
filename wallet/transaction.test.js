2aed8371a427bd237aac6599621aefb1218b669f
Douno <douno30@gmail.com>
2022-01-30 09:57:53 +0000

    Tested input amount

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -15,12 +15,16 @@ describe('Transaction', () => {
   it('outputs the `amount` subtracted from the wallet balance', () => {
     expect(transaction.outputs.find(output => output.address === wallet.publicKey).amount)
       .toEqual(wallet.balance - amount);
-  })
+  });
 
   it('outputs the `amount` added to the recipient', () => {
     expect(transaction.outputs.find(output => output.address === recipient).amount)
       .toEqual(amount);
-  })
+  });
+
+  it('inputs the balance of the wallet', () => {
+    expect(transaction.input.amount).toEqual(wallet.balance);
+  });
 
   describe('transaction with an amount that exceeds the balance', () => {
     beforeEach(() => {
